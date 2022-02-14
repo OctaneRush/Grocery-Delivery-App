831e0518dcf7f4db39a2e16ba9ef9f270a1a1c16
Zachery Younggren <zwyounggren1@buffs.wtamu.edu>
2022-02-13 20:39:03 -0600

    Test file Added

---
+++ Grocery-Delivery-Test/GroceryTest/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GroceryTest{
+    public class GroceryTest
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
+public class Class1
+{
+
+}
